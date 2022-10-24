ba7cc3e7a3ee309b84b3c1bb189856f786671eb4
Дмитрий <zakon.protiv.us@gmail.com>
2022-10-24 16:19:10 +0700

    new skeleton

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-const box = document.getElementById("box");
+const rangeOfBoxes = document.querySelector("#range-of-boxes");
+const sketch = document.querySelector("#box");
 const div = document.createElement("div");
-const rangeOfBoxes = document.getElementById("range-of-boxes");
-let squares
 
-rangeOfBoxes.addEventListener("input", (e) => {
-    box.innerHTML = "";
-    for(let i = 1; i<=rangeOfBoxes.value**2; i++) {
-        box.appendChild(div.cloneNode())
-    }
-    box.style.gridTemplateColumns = `repeat(${rangeOfBoxes.value},1fr)`;
-    box.style.gridTemplateRows = `repeat(${rangeOfBoxes.value},1fr)`;
-    squares = Array.from(box.children);
-    for(let i = 0; i<squares.length; i++) {
-        squares[i].addEventListener("mouseover", (e) => {
-            e.target.classList.toggle("color");
-        })
+rangeOfBoxes.addEventListener("input",e => {
+    changeAmountOfBoxes(e.target.value);
+})
+
+sketch.addEventListener("mouseover", e => {
+    if( e.target.classList[0] != "box") {
+        e.target.classList.toggle("color");
     }
 })
 
 
 
-
-console.log(rangeOfBoxes.value);
+function changeAmountOfBoxes(amount) {
+    sketch.innerHTML = "";
+    while(+sketch.childElementCount < (+amount)**2) {
+        sketch.appendChild(div.cloneNode());
+    }
+    sketch.style.gridTemplateColumns = `repeat(${+amount},1fr)`;
+    sketch.style.gridTemplateRows = `repeat(${+amount},1fr)`;
+    console.log(sketch.childElementCount);
+}
+changeAmountOfBoxes(50)
